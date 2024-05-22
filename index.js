fbff8f289eff65053d2a952305f8c546adbec309
Jalen Pinder <jtp1326@gmail.com>
2024-05-22 17:24:17 -0300

    Consolidate feed.ejs into index.ejs, handle rotuing for About and Contact pages

--- index.js
+++ index.js
@@ -12,6 +12,22 @@ app.get("/", (req, res) => {
     res.render("index.ejs");
 })
 
+app.get("/about", (req, res) => {
+    res.render("about.ejs");
+})
+
+app.get("/contact", (req, res) => {
+    res.render("contact.ejs");
+})
+
+app.post("/update-feed", (req, res) => {
+    res.render("index.ejs");
+})
+
+app.patch("/edit-post", (req, res) => {
+    res.render("index.ejs");
+})
+
 app.listen(port, () => {
     console.log(`Server running on port ${port}.`)
 });
