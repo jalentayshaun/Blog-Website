2a2cadbb9937f05436dbdd99e14f2b9313635eb0
Jalen Pinder <jtp1326@gmail.com>
2024-05-23 00:32:58 -0300

    Add more server request handlers

--- index.js
+++ index.js
@@ -8,22 +8,32 @@ app.use(express.static("public"));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Home
 app.get("/", (req, res) => {
     res.render("index.ejs");
 })
 
+// About
 app.get("/about", (req, res) => {
     res.render("about.ejs");
 })
 
+// Contact page
 app.get("/contact", (req, res) => {
     res.render("contact.ejs");
 })
 
+// Contact form message
+app.post("/contact-message", (req, res) => {
+    res.render("index.ejs");
+})
+
+// Post to feed
 app.post("/update-feed", (req, res) => {
     res.render("index.ejs");
 })
 
+// Edit post
 app.patch("/edit-post", (req, res) => {
     res.render("index.ejs");
 })
