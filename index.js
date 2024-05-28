d78b622924ca77bf53560d25e25a5860f7bfe98c
Jalen Pinder <jtp1326@gmail.com>
2024-05-28 12:11:57 -0300

    Improve passing data from server to index.ejs. Need to fix posting to feed

--- index.js
+++ index.js
@@ -31,19 +31,25 @@ app.post("/contact-message", (req, res) => {
 // Post to feed
 var feedArray = [];
 app.post("/update-feed", (req, res) => {
+
     var currentTime = new Date().toLocaleString();
     var subject = req.body["subject"];
     var message = req.body["post-message"];
 
     const data = {
+        updateSuccessful: true,
         username: `<span class="username">colorful_clove</span>`,
         profilePicture: `<img src="main/images/svgs/user-regular.svg" alt="User Icon">`,
         timeStamp: `<small class="time-stamp">${currentTime}</small>`,
         postSubject: `<h3>${subject}</h3>`,
         postMessage: `<p>${message}</p>`
     };
+
+    feedArray.push(data);
+    console.log(feedArray.length);
+    console.log(feedArray);
     
-    res.render("index.ejs", data);
+    res.render("index.ejs", {data, feedArray});
 })
 
 // Edit post
